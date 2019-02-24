8ea2bfa6d83cab97de3d6ee58fab952ca79ac19a
DDumas551 <david.j.dumas@gmail.com>
2019-02-24 05:07:26 -0800

    Adds table to test mobile layout

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,13 +1,14 @@
 var config = {
-    apiKey: "AIzaSyBe3mNsWHc0hcOoeQL8rlj0j9kdh6oqqxY",
-    authDomain: "schedulingapp-b9236.firebaseapp.com",
-    databaseURL: "https://schedulingapp-b9236.firebaseio.com",
-    projectId: "schedulingapp-b9236",
-    storageBucket: "",
-    messagingSenderId: "113794503519"
-  };
-  firebase.initializeApp(config);
+  apiKey: "AIzaSyBe3mNsWHc0hcOoeQL8rlj0j9kdh6oqqxY",
+  authDomain: "schedulingapp-b9236.firebaseapp.com",
+  databaseURL: "https://schedulingapp-b9236.firebaseio.com",
+  projectId: "schedulingapp-b9236",
+  storageBucket: "schedulingapp-b9236.appspot.com",
+  messagingSenderId: "113794503519"
+};
+firebase.initializeApp(config);
 
+let database = firebase.database();
 
 $("#customerSaveButton").on("click", () => {
   let customerFirstName = $("#customerFirstName").val();
@@ -17,12 +18,50 @@ $("#customerSaveButton").on("click", () => {
   let customerStreetAddress = $("#customerStreetAddress").val();
   let customerCityAddress = $("#customerCityAddress").val();
   let customerStateAddress = $("#customerStateAddress").val();
+  let customerStreetAddress2 = $("#customerStreetAddress2").val();
+  let customerCityAddress2 = $("#customerCityAddress2").val();
+  let customerStateAddress2 = $("#customerStateAddress2").val();
+  let customerStreetAddress3 = $("#customerStreetAddress3").val();
+  let customerCityAddress3 = $("#customerCityAddress3").val();
+  let customerStateAddress3 = $("#customerStateAddress3").val();
   let customerTime = $("#customerTime").val();
   let customerDate = $("#customerDate").val();
   let customerSuppliesNeeded = $("#customerSuppliesNeeded").val();
+  let depositAmt = $("#depositAmt").val();
+  let hourlyRate = $("#hourlyRate").val();
+  let paymentMethodFB = $("#paymentMethod").val();
+
+  database.ref().push({
+    customerFirstName: customerFirstName,
+    customerLastName: customerLastName,
+    customerPhoneNumber: customerPhoneNumber,
+    customerAltPhoneNumber: customerAltPhoneNumber,
+    customerStreetAddress: customerStreetAddress,
+    customerCityAddress: customerCityAddress,
+    customerStateAddress: customerStateAddress,
+    customerStreetAddress2: customerStreetAddress2,
+    customerCityAddress2: customerCityAddress2,
+    customerStateAddress2: customerStateAddress2,
+    customerStreetAddress3: customerStreetAddress3,
+    customerCityAddress3: customerCityAddress3,
+    customerStateAddress3: customerStateAddress3,
+    customerTime: customerTime,
+    customerDate: customerDate,
+    customerSuppliesNeeded: customerSuppliesNeeded,
+    depositAmt: depositAmt,
+    hourlyRate: hourlyRate,
+    paymentMethodFB: paymentMethodFB
+
+  });
 });
 
+// database.ref().on("child_added", function(childSnapshot) {
+
+// });
+
+// let jObject {[]};
 let carrierSelected;
+let paymentMethod;
 
 
 let cellCarrier = {
@@ -49,6 +88,11 @@ PagePlus: "@vtext.com"
 $(".cellItem").on("click", function() {
   $(".carrierSelected").text(this.text);
   carrierSelected = this.text;
+  });
+
+$(".paymentMethod").on("click", function() {
+  $("#paymentMethod").text(this.text);
+  paymentMethod = this.text;
   });
   
 
